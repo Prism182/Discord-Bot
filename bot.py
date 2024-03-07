808bb795d8b4a2c075dc23cbf87aff2cc4fcecab
Prism182 <kylefos@icloud.com>
2024-03-07 18:09:13 +0000

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import lightbulb
 bot = lightbulb.BotApp(
     "MTE5NzY2MTYzNzE4MDY3NDA4OQ.G218Z0.JV_sizHI4YB4pxq4zNctIruMIfv58cE8bU7mKg",
 
-    #default_enabled_guilds=(1197653145745104967),
+    default_enabled_guilds=(1197653145745104967),
 
     intents=hikari.Intents.ALL_UNPRIVILEGED  # Add this
     | 
