7c2d9e273b44a8fab865cccf0e7e9fee90f9bf3b
Prism182 <kylefos@icloud.com>
2024-03-08 22:22:26 +0000

    modified:   extensions/listen.py

--- extensions/listen.py
+++ extensions/listen.py
@@ -7,23 +7,11 @@ plugin = lightbulb.Plugin("Listen")
 
 @plugin.listener(hikari.GuildMessageDeleteEvent)
 async def print_messages(event):
-    await event.delete(event.listener)
+    pass
 
 @plugin.listener(hikari.GuildMessageUpdateEvent)
 async def message_edit_logger(event):
-
-    old_message = await event.message.get_old()
-
-    if old_message:
-        old_content = event.old_content
-        new_content = event.new_content
-        channel = event.message.get_channel()
-        author = event.message.author
-
-        print(f"Message updated by {author.username}:")
-        print(f"Old content: {old_content}")
-        print(f"New content: {new_content}")
-
+    pass
 @plugin.listener(lightbulb.CommandErrorEvent)
 async def on_error(event: lightbulb.CommandErrorEvent) -> None:
     ...
