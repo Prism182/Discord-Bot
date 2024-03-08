f13c4278416293230d59da7bd05fef3e5b13bfa8
Prism182 <kylefos@icloud.com>
2024-03-08 22:18:54 +0000

    modified:   bot.py 	modified:   extensions/listen.py

--- bot.py
+++ bot.py
@@ -27,29 +27,34 @@ async def ping(ctx):
         ]
     )
 
-folder_path = "./zipbomb"
-file_name2 = "./Zipbomb.zip"
-
-#@bot.command
-#@lightbulb.option("mb", "How large the zip bomb file is times 10", type=int)
-#@lightbulb.command("createzb", "This creates a zip bomb and sends it to you, maximum file size is 100Mb")
-#@lightbulb.implements(lightbulb.SlashCommand)
-#async def zipb(ctx):
-#    if ctx.options.mb <= 10:
-#        await ctx.respond("creating Zipbomb")
-#        if not os.path.exists(folder_path):
-#            os.makedirs(folder_path)
-#        for i in range(0, ctx.options.mb):
-#            file_name = str(i)
-#            file_path = os.path.join(folder_path, file_name + ".txt")
-#            with open(file_path, "w") as f:
-#                f.write(file_name*100000000)
-#        shutil.make_archive("Zipbomb", "zip", folder_path)
-#        shutil.copyfile(folder_path, "Zipbomb.zip")
-#        await ctx.respond("Zip file created; sending Zip file")
-#        with open("./Zipbomb.zip", "rb") as f:
-#            await ctx.respond(file=hikari.File(f))
-#    else:
-#        await ctx.respond("Please try again as the entered amount is too large")
+def close():
+    #
+    #
+    #folder_path = "./zipbomb"
+    #file_name2 = "./Zipbomb.zip"
+    #
+    #
+    #@bot.command
+    #@lightbulb.option("mb", "How large the zip bomb file is times 10", type=int)
+    #@lightbulb.command("createzb", "This creates a zip bomb and sends it to you, maximum file size is 100Mb")
+    #@lightbulb.implements(lightbulb.SlashCommand)
+    #async def zipb(ctx):
+    #    if ctx.options.mb <= 10:
+    #        await ctx.respond("creating Zipbomb")
+    #        if not os.path.exists(folder_path):
+    #            os.makedirs(folder_path)
+    #        for i in range(0, ctx.options.mb):
+    #            file_name = str(i)
+    #            file_path = os.path.join(folder_path, file_name + ".txt")
+    #            with open(file_path, "w") as f:
+    #                f.write(file_name*100000000)
+    #        shutil.make_archive("Zipbomb", "zip", folder_path)
+    #        shutil.copyfile(folder_path, "Zipbomb.zip")
+    #        await ctx.respond("Zip file created; sending Zip file")
+    #        with open("./Zipbomb.zip", "rb") as f:
+    #            await ctx.respond(file=hikari.File(f))
+    #    else:
+    #        await ctx.respond("Please try again as the entered amount is too large")
+    return
 
 bot.run()
