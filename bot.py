6c81617c02f1cfa1dd233d6dcdf46b4ce6a8f279
Prism182 <162613993+Prism182@users.noreply.github.com>
2024-03-22 11:56:27 +0000

    Update bot.py
    Cleaned up unnecessary code

--- bot.py
+++ bot.py
@@ -39,36 +39,6 @@ async def ping(ctx):
         ]
     )
 
-def close():
-    #
-    #
-    #folder_path = "./zipbomb"
-    #file_name2 = "./Zipbomb.zip"
-    #
-    #
-    #@bot.command
-    #@lightbulb.option("mb", "How large the zip bomb file is times 10", type=int)
-    #@lightbulb.command("createzb", "This creates a zip bomb and sends it to you, maximum file size is 100Mb")
-    #@lightbulb.implements(lightbulb.SlashCommand)
-    #async def zipb(ctx):
-    #    if ctx.options.mb <= 10:
-    #        await ctx.respond("creating Zipbomb")
-    #        if not os.path.exists(folder_path):
-    #            os.makedirs(folder_path)
-    #        for i in range(0, ctx.options.mb):
-    #            file_name = str(i)
-    #            file_path = os.path.join(folder_path, file_name + ".txt")
-    #            with open(file_path, "w") as f:
-    #                f.write(file_name*100000000)
-    #        shutil.make_archive("Zipbomb", "zip", folder_path)
-    #        shutil.copyfile(folder_path, "Zipbomb.zip")
-    #        await ctx.respond("Zip file created; sending Zip file")
-    #        with open("./Zipbomb.zip", "rb") as f:
-    #            await ctx.respond(file=hikari.File(f))
-    #    else:
-    #        await ctx.respond("Please try again as the entered amount is too large")
-    return
-
 @bot.command
 @lightbulb.option("channel_name", "the channel id in which you want to purge", type=str)
 @lightbulb.option("days", "sets how many days worth of messages you want to delete 0 = today", type=int)
